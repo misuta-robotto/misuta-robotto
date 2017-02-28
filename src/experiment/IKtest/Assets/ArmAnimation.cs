dcf1332f36ee6023510623e14014589911385c59
Emil Segerbäck <emilsegerback@gmail.com>
2017-02-28 12:29:54 +0100

    Add IK test project

---
+++ src/experiment/IKtest/Assets/ArmAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAnimation : MonoBehaviour {
+    public Transform objToPickUp = null;
+    private Animator animator;
+
+	void Start() {
+        animator = GetComponent<Animator>();
+	}
+
+	void OnAnimatorIK(int layerIndex) {
+        if (objToPickUp != null) {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+
+            animator.SetIKPosition(AvatarIKGoal.RightHand, objToPickUp.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, objToPickUp.rotation);
+        }
+	}
+}
