f9203feb9645e4e3f1a390b42ca1f8c68ecff8c0
Emil Segerbäck <emilsegerback@gmail.com>
2017-03-23 11:30:16 +0100

    Remove triangle and change background for camera demo

--- src/experiment/graphicsdemos/NativeRenderingPlugin/PluginSource/source/RenderingPlugin.cpp
+++ src/experiment/graphicsdemos/NativeRenderingPlugin/PluginSource/source/RenderingPlugin.cpp
@@ -13,9 +13,6 @@
 // --------------------------------------------------------------------------
 // SetTimeFromUnity, an example function we export which is called by one of the scripts.
 
-static float g_Time;
-
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetTimeFromUnity (float t) { g_Time = t; }
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetDevice(int dev);
 
 static cv::Mat frame;
@@ -61,52 +58,6 @@ extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetTextureFromUnity(v
 	g_TextureHandle = textureHandle;
 	g_TextureWidth = w;
 	g_TextureHeight = h;
-}
-
-
-// --------------------------------------------------------------------------
-// SetMeshBuffersFromUnity, an example function we export which is called by one of the scripts.
-
-static void* g_VertexBufferHandle = NULL;
-static int g_VertexBufferVertexCount;
-
-struct MeshVertex
-{
-	float pos[3];
-	float normal[3];
-	float uv[2];
-};
-static std::vector<MeshVertex> g_VertexSource;
-
-
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetMeshBuffersFromUnity(void* vertexBufferHandle, int vertexCount, float* sourceVertices, float* sourceNormals, float* sourceUV)
-{
-	// A script calls this at initialization time; just remember the pointer here.
-	// Will update buffer data each frame from the plugin rendering event (buffer update
-	// needs to happen on the rendering thread).
-	g_VertexBufferHandle = vertexBufferHandle;
-	g_VertexBufferVertexCount = vertexCount;
-
-	// The script also passes original source mesh data. The reason is that the vertex buffer we'll be modifying
-	// will be marked as "dynamic", and on many platforms this means we can only write into it, but not read its previous
-	// contents. In this example we're not creating meshes from scratch, but are just altering original mesh data --
-	// so remember it. The script just passes pointers to regular C# array contents.
-	g_VertexSource.resize(vertexCount);
-	for (int i = 0; i < vertexCount; ++i)
-	{
-		MeshVertex& v = g_VertexSource[i];
-		v.pos[0] = sourceVertices[0];
-		v.pos[1] = sourceVertices[1];
-		v.pos[2] = sourceVertices[2];
-		v.normal[0] = sourceNormals[0];
-		v.normal[1] = sourceNormals[1];
-		v.normal[2] = sourceNormals[2];
-		v.uv[0] = sourceUV[0];
-		v.uv[1] = sourceUV[1];
-		sourceVertices += 3;
-		sourceNormals += 3;
-		sourceUV += 2;
-	}
 }
 
 
@@ -187,40 +138,6 @@ static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType ev
 // be the integer passed to IssuePluginEvent. In this example, we just ignore
 // that value.
 
-
-static void DrawColoredTriangle()
-{
-	// Draw a colored triangle. Note that colors will come out differently
-	// in D3D and OpenGL, for example, since they expect color bytes
-	// in different ordering.
-	struct MyVertex
-	{
-		float x, y, z;
-		unsigned int color;
-	};
-	MyVertex verts[3] =
-	{
-		{ -0.5f, -0.25f,  0, 0xFFff0000 },
-		{ 0.5f, -0.25f,  0, 0xFF00ff00 },
-		{ 0,     0.5f ,  0, 0xFF0000ff },
-	};
-
-	// Transformation matrix: rotate around Z axis based on time.
-	float phi = g_Time; // time set externally from Unity script
-	float cosPhi = cosf(phi);
-	float sinPhi = sinf(phi);
-	float depth = 0.7f;
-	float finalDepth = s_CurrentAPI->GetUsesReverseZ() ? 1.0f - depth : depth;
-	float worldMatrix[16] = {
-		cosPhi,-sinPhi,0,0,
-		sinPhi,cosPhi,0,0,
-		0,0,1,0,
-		0,0,finalDepth,1,
-	};
-
-	s_CurrentAPI->DrawSimpleTriangles(worldMatrix, 1, verts);
-}
-
 static void CopyCamPicture(void* textureDataPtr, int textureRowPitch)
 {
     cap.read(frame);
@@ -232,16 +149,6 @@ static void CopyCamPicture(void* textureDataPtr, int textureRowPitch)
 
 	int width = g_TextureWidth;
 	int height = g_TextureHeight;
-
-	// glTexImage2D(GL_TEXTURE_2D,     // Type of texture
-	//              0,                 // Pyramid level (for mip-mapping) - 0 is the top level
-	//              GL_RGB,            // Internal colour format to convert to
-	//              mat.cols,          // Image width  i.e. 640 for Kinect in standard mode
-	//              mat.rows,          // Image height i.e. 480 for Kinect in standard mode
-	//              0,                 // Border width in pixels (can either be 1 or 0)
-	//              inputColourFormat, // Input image format (i.e. GL_RGB, GL_RGBA, GL_BGR etc.)
-	//              GL_UNSIGNED_BYTE,  // Image data type
-	//              mat.ptr());        // The actual image data itself
 
     const int opencv_bpp = 3;
 
@@ -281,76 +188,9 @@ static void ModifyTexturePixels()
 	if (!textureDataPtr)
 		return;
 
-	const float t = g_Time * 4.0f;
-
     CopyCamPicture(textureDataPtr, textureRowPitch);
 
-	// unsigned char* dst = (unsigned char*)textureDataPtr;
-	// for (int y = 0; y < height; ++y)
-	// {
-	// 	unsigned char* ptr = dst;
-	// 	for (int x = 0; x < width; ++x)
-	// 	{
-	// 		// Simple "plasma effect": several combined sine waves
-	// 		int vv = int(
-	// 			(127.0f + (127.0f * sinf(x / 7.0f + t))) +
-	// 			(127.0f + (127.0f * sinf(y / 5.0f - t))) +
-	// 			(127.0f + (127.0f * sinf((x + y) / 6.0f - t))) +
-	// 			(127.0f + (127.0f * sinf(sqrtf(float(x*x + y*y)) / 4.0f - t)))
-	// 			) / 4;
-
-	// 		// Write the texture pixel
-	// 		ptr[0] = vv;
-	// 		ptr[1] = vv;
-	// 		ptr[2] = vv;
-	// 		ptr[3] = vv;
-
-	// 		// To next pixel (our pixels are 4 bpp)
-	// 		ptr += 4;
-	// 	}
-
-	// 	// To next image row
-	// 	dst += textureRowPitch;
-	// }
-
 	s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, textureDataPtr);
-}
-
-
-static void ModifyVertexBuffer()
-{
-	void* bufferHandle = g_VertexBufferHandle;
-	int vertexCount = g_VertexBufferVertexCount;
-	if (!bufferHandle)
-		return;
-
-	size_t bufferSize;
-	void* bufferDataPtr = s_CurrentAPI->BeginModifyVertexBuffer(bufferHandle, &bufferSize);
-	if (!bufferDataPtr)
-		return;
-	int vertexStride = int(bufferSize / vertexCount);
-
-	const float t = g_Time * 3.0f;
-
-	char* bufferPtr = (char*)bufferDataPtr;
-	// modify vertex Y position with several scrolling sine waves,
-	// copy the rest of the source data unmodified
-	for (int i = 0; i < vertexCount; ++i)
-	{
-		const MeshVertex& src = g_VertexSource[i];
-		MeshVertex& dst = *(MeshVertex*)bufferPtr;
-		dst.pos[0] = src.pos[0];
-		dst.pos[1] = src.pos[1];
-		dst.pos[2] = src.pos[2];
-		dst.normal[0] = src.normal[0];
-		dst.normal[1] = src.normal[1];
-		dst.normal[2] = src.normal[2];
-		dst.uv[0] = src.uv[0];
-		dst.uv[1] = src.uv[1];
-		bufferPtr += vertexStride;
-	}
-
-	s_CurrentAPI->EndModifyVertexBuffer(bufferHandle);
 }
 
 
@@ -360,9 +200,7 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 	if (s_CurrentAPI == NULL)
 		return;
 
-	DrawColoredTriangle();
 	ModifyTexturePixels();
-	ModifyVertexBuffer();
 }
 
 
--- src/experiment/graphicsdemos/NativeRenderingPlugin/UnityProject/Assets/UseRenderingPlugin.cs
+++ src/experiment/graphicsdemos/NativeRenderingPlugin/UnityProject/Assets/UseRenderingPlugin.cs
@@ -45,7 +45,6 @@ public class UseRenderingPlugin : MonoBehaviour
     IEnumerator Start()
 	{
 		CreateTextureAndPassToPlugin();
-		SendMeshBuffersToPlugin();
         InitOpenCV();
         number_of_devices = GetDeviceCount();
         current_device = 0;
@@ -76,7 +75,7 @@ public class UseRenderingPlugin : MonoBehaviour
     private void CreateTextureAndPassToPlugin()
 	{
 		// Create a texture
-		Texture2D tex = new Texture2D(1920,1080,TextureFormat.ARGB32,false);
+		Texture2D tex = new Texture2D(1920, 1080, TextureFormat.ARGB32, false);
 		// Set point filtering just so we can see the pixels clearly
 		tex.filterMode = FilterMode.Point;
 		// Call Apply() so it's actually uploaded to the GPU
@@ -86,46 +85,14 @@ public class UseRenderingPlugin : MonoBehaviour
 		GetComponent<Renderer>().material.mainTexture = tex;
 
 		// Pass texture pointer to the plugin
-		SetTextureFromUnity (tex.GetNativeTexturePtr(), tex.width, tex.height);
+		SetTextureFromUnity(tex.GetNativeTexturePtr(), tex.width, tex.height);
 	}
-
-	private void SendMeshBuffersToPlugin ()
-	{
-		var filter = GetComponent<MeshFilter> ();
-		var mesh = filter.mesh;
-		// The plugin will want to modify the vertex buffer -- on many platforms
-		// for that to work we have to mark mesh as "dynamic" (which makes the buffers CPU writable --
-		// by default they are immutable and only GPU-readable).
-		mesh.MarkDynamic ();
-
-		// However, mesh being dynamic also means that the CPU on most platforms can not
-		// read from the vertex buffer. Our plugin also wants original mesh data,
-		// so let's pass it as pointers to regular C# arrays.
-		// This bit shows how to pass array pointers to native plugins without doing an expensive
-		// copy: you have to get a GCHandle, and get raw address of that.
-		var vertices = mesh.vertices;
-		var normals = mesh.normals;
-		var uvs = mesh.uv;
-		GCHandle gcVertices = GCHandle.Alloc (vertices, GCHandleType.Pinned);
-		GCHandle gcNormals = GCHandle.Alloc (normals, GCHandleType.Pinned);
-		GCHandle gcUV = GCHandle.Alloc (uvs, GCHandleType.Pinned);
-
-		SetMeshBuffersFromUnity (mesh.GetNativeVertexBufferPtr (0), mesh.vertexCount, gcVertices.AddrOfPinnedObject (), gcNormals.AddrOfPinnedObject (), gcUV.AddrOfPinnedObject ());
-
-		gcVertices.Free ();
-		gcNormals.Free ();
-		gcUV.Free ();
-	}
-
 
 	private IEnumerator CallPluginAtEndOfFrames()
 	{
 		while (true) {
 			// Wait until all frame rendering is done
 			yield return new WaitForEndOfFrame();
-
-			// Set time for the plugin
-			SetTimeFromUnity (Time.timeSinceLevelLoad);
 
 			// Issue a plugin event with arbitrary integer identifier.
 			// The plugin can distinguish between different
@@ -134,6 +101,4 @@ public class UseRenderingPlugin : MonoBehaviour
 			GL.IssuePluginEvent(GetRenderEventFunc(), 1);
 		}
 	}
-
-
 }
