122a2d4b0424348e6482e9467866c30ea84a4c9d
robch386 <robch386@student.liu.se>
2017-03-24 12:49:46 +0100

    Updated position movement (not tested)

--- src/Product/Assets/Scripts/Behaviours/PadMove.cs
+++ src/Product/Assets/Scripts/Behaviours/PadMove.cs
@@ -8,24 +8,52 @@ using UnityEngine;
 using UnityEngine.VR;
 
 public class PadMove : MonoBehaviour {
-     private ALMotionProxy motionProxy;
-     //private SteamVR_TrackedObject trackedObj;
-     //private SteamVR_Controller.Device Controller
-     /*{
-	      get { return SteamVR_Controller.Input((int)trackedObj.index); }
-     }*/
+    private ALMotionProxy motionProxy;
+    private SteamVR_TrackedObject trackedObjLeft;
+    private SteamVR_Controller.Device ControllerLeft {
+	    get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    private SteamVR_TrackedObject trackedObjRight;
+    private SteamVR_Controller.Device ControllerRight {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
 
-     void Start () {
+    void Start () {
         motionProxy = new ALMotionProxy("127.0.0.1", 2104);
         Debug.Log("Started ");
         motionProxy.MoveInit();
      }
 
-     /*void Awake () {
-     	trackedObj = GetComponent<SteamVR_TrackedObject>();
-     }*/
+     void Awake () {
+     	trackedObjLeft = GetComponent<SteamVR_TrackedObject>();
+        trackedObjRight = GetComponent<SteamVR_TrackedObject>();
+     }
 
      void Update () {
-        
-     }
+        // Ej testat!
+        float x = 0;
+        float y = 0;
+        float theta = 0;
+
+        // ControllerLeft - Förflyttning framåt och bakåt
+        if (System.Math.Abs(ControllerLeft.GetAxis().y) > 0.2)
+        {
+            x = ControllerLeft.GetAxis().y;
+        }
+
+        // ControllerLeft - Sidledsförflyttning
+        if (System.Math.Abs(ControllerLeft.GetAxis().x) > 0.2)
+        {
+            y = ControllerLeft.GetAxis().x;
+        }
+
+        // ControllerRight - Rotation
+        if (System.Math.Abs(ControllerRight.GetAxis().y) > 0.2)
+        {
+            theta = -ControllerRight.GetAxis().y;
+        }
+
+        motionProxy.Move(x, y, theta);
+        //Debug.Log(gameObject.name + Controller.GetAxis());
+    }
 }
