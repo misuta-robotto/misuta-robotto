fbe4e6a1225ff174a72930f0ca6a2ce59da565a6
Patrik Sletmo <patsl736@student.liu.se>
2017-04-06 16:32:40 +0200

    Avoid duplicate bridge code by moving all to Product assets

--- src/Product/Assets/Scripts/Robot/ALMotionProxy.cs
+++ src/Product/Assets/Scripts/Robot/ALMotionProxy.cs
@@ -4,36 +4,41 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 
 namespace AL {
+    // ALMotionProxy is used to communicate with a robot and perform
+    // various motion actions.
+    // 
+    // For more information see
+    // http://doc.aldebaran.com/2-1/naoqi/motion/control-cartesian-api.html
     public class ALMotionProxy {
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern IntPtr ALMotionProxyNew(string server, int port);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern void ALMotionProxyAngleInterpolation(IntPtr self, IntPtr names, IntPtr angleList, IntPtr timeLists, bool isAbsolute);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern void ALMotionProxyAngleInterpolationWithSpeed(IntPtr self, IntPtr names, IntPtr targetAngles, float maxSpeedFraction);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern void ALMotionProxyAngleInterpolationBezier(IntPtr self, IntPtr jointNames, IntPtr times, IntPtr controlPoints);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern void ALMotionProxySetAngles(IntPtr self, IntPtr names, IntPtr angles, float fractionMaxSpeed);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern void ALMotionProxyChangeAngles(IntPtr self, IntPtr names, IntPtr changes, float fractionMaxSpeed);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern IntPtr ALMotionProxyGetAngles(IntPtr self, IntPtr names, bool useSensors);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern IntPtr ALMotionProxyCloseHand(IntPtr self, string handName);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge_d.dll")]
         private static extern IntPtr ALMotionProxyOpenHand(IntPtr self, string handName);
 
-        [DllImport("bridge_d")]
-        private static extern IntPtr ALMotionProxyFree(IntPtr self);
+        [DllImport("bridge_d.dll")]
+        private static extern IntPtr AlMotionProxyFree(IntPtr self);
 
         [DllImport("bridge_d")]
         private static extern void ALMotionProxyMoveInit(IntPtr self);
@@ -55,7 +60,7 @@ namespace AL {
 
         ~ALMotionProxy()
         {
-            ALMotionProxyFree(unmanagedMem);
+            AlMotionProxyFree(unmanagedMem);
         }
 
         public void AngleInterpolation(string[] names, float[] angles, float[] timeLists, bool isAbsolute)
@@ -98,7 +103,7 @@ namespace AL {
                 fractionMaxSpeed
             );
         }
-        
+
         public void ChangeAngles(string[] names, float[] changes, float fractionMaxSpeed)
         {
             ALMotionProxyChangeAngles(
--- src/Product/Assets/Scripts/Robot/ALValue.cs
+++ src/Product/Assets/Scripts/Robot/ALValue.cs
@@ -2,20 +2,28 @@ using System;
 using System.Runtime.InteropServices;
 
 namespace AL {
+    // ALValue is a container object used in the Aldebaran SDK.
+    // Because of its container nature it does not contain any type
+    // information. The C# implementation uses ALValue internally to
+    // call the rest of the Aldebaran SDK which means that any consumer
+    // of this library must not bother with this limitation.
+    //
+    // For more information see
+    // http://doc.aldebaran.com/2-4/ref/libalvalue/classAL_1_1ALValue.html     
     public class ALValue {
-        [DllImport("bridge_d")]
+        [DllImport("bridge")]
         private static extern IntPtr ALValue_f(float value);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge")]
         private static extern IntPtr ALValue_s(string value);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge")]
         private static extern IntPtr ALValue_fv(float[] values, int numValues);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge")]
         private static extern IntPtr ALValue_sv(string[] value, int numValues);
 
-        [DllImport("bridge_d")]
+        [DllImport("bridge")]
         private static extern void ALValueFree(IntPtr memory);
 
         public IntPtr Pointer
